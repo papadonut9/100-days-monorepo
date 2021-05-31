a85e4d291756ab79f1baa0535966545c9d0824bc
dolor3s <anchit.m@somaiya.edu>
2021-06-01 03:13:01 +0530

    version 0.1.1

---
+++ 5-Password-Generator/README.md
@@ -0,0 +1,8 @@
+# Password Generator
+
+### Changelog:
+
+ `v0.1.1` 
+ - added basic functionality
+ - Users can fine control over how many letters, numbers and characters they want in their password string
+ 
--- 5-Password-Generator/src/main.py
+++ 5-Password-Generator/src/main.py
@@ -1,4 +1,5 @@
 import random
+import time as t
 
 lettersList = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y',
                'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
@@ -6,4 +7,31 @@ lettersList = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 
 numList = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
 
 symList = ['!', '@', '#', '$', '%', '^', '&', '*',
-           '(', ')', '-', '_', '=', '+', '[', '{', ']', '}', '|', '\\', ';', ':', '\'', '"', ',', '<', '.', '>', '/', '?', '`', '~']
+           '(', ')', '-', '_', '=', '+', '[', '{', ']', '}', '|',  ';', ':', '\'', '"', ',', '<', '.', '>', '/', '?', '`', '~']
+
+
+length = int(input('Enter the password length: ')) # Useless at this version
+charLength = int(
+    input('Enter how many characters you want in your password: '))
+numLength = int(input('Enter how many numbers you want in your password: '))
+symLength = int(input('Enter how many characters you want in your password: '))
+
+password = ''
+generatedString = []
+
+for character in range(0, charLength):
+    generatedString.append(random.choice(lettersList))
+
+for number in range(0, numLength):
+    generatedString.append(random.choice(numList))
+
+for symbol in range(0, symLength):
+    generatedString.append(random.choice(symList))
+
+print('Generated password, now shuffling to increase entropy:')
+t.sleep(random.randint(2, 7))
+random.shuffle(generatedString)
+
+for character in generatedString:
+    password += character
+print(f'Your generated password is: {password}')
