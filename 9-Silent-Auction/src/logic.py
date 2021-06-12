761cb151b9ccd608f294d4c9d7256bc332a7bb06
dolor3s <anchit.m@somaiya.edu>
2021-06-13 01:49:10 +0530

    9- v1.0.1 Stable

--- 9-Silent-Auction/src/logic.py
+++ 9-Silent-Auction/src/logic.py
@@ -1,10 +1,37 @@
-bids = []
+import os
 
 
 # noinspection SpellCheckingInspection
-def auctionInput(name='', bidval=0.00):
+def clrscr():
+    if os.name == 'posix':
+        os.system('clear')
+    elif os.name == 'nt':
+        os.system('cls')
+
+
+# noinspection SpellCheckingInspection
+def auctionInput(bid_db, name='', bidval=0.00, ):
     application = {
         'name': name,
         'bid': bidval
     }
-    bids.append(application)
+    bid_db.append(application)
+
+
+# noinspection SpellCheckingInspection
+def declareWinner(bid_db):
+    maxV = 0.00
+    winner = ''
+    for bid in bid_db:
+        if bid['bid'] > maxV:
+            maxV = bid['bid']
+            winner = bid['name']
+    return f'The winner of the Auction is {winner} with a winning bid of {maxV}'
+
+
+# Testing
+# import auction_db
+#
+# bids = auction_db.bids
+# print(declareWinner(bids))
+# clrscr()
--- 9-Silent-Auction/src/main.py
+++ 9-Silent-Auction/src/main.py
@@ -1,29 +1,25 @@
 import logic
-
-print("""
-  ____   _  _               _                 
- / ___| (_)| |  ___  _ __  | |_               
- \___ \ | || | / _ \| '_ \ | __|              
-  ___) || || ||  __/| | | || |_               
- |____/ |_||_| \___||_| |_| \__|              
-       _                 _    _               
-      / \   _   _   ___ | |_ (_)  ___   _ __  
-     / _ \ | | | | / __|| __|| | / _ \ | '_ \ 
-    / ___ \| |_| || (__ | |_ | || (_) || | | |
-   /_/   \_\\\\__,_| \___| \__||_| \___/ |_| |_|
-                                              
-""")
-
+import auction_db
+import ui
 moreParticipants = True
+db = auction_db.bids
 
+ui.splash()
 while moreParticipants:
     name = input('Enter your name: ')
-    bid = float(input('Enter your bid: '))
-    stillContinue = input('Are there any other members? Type yes/no').lower()
+    bid = round(float(input('Enter your bid: ')), 2)
+    logic.auctionInput(db, name, bid)
+    stillContinue = input('Are there any other members? Type yes/no: ').lower()
 
     if stillContinue == 'no':
         moreParticipants = False
     elif stillContinue == 'yes':
+        logic.clrscr()
         moreParticipants = True
+        ui.splash()
     else:
         print('invalid input')
+        logic.clrscr()
+        ui.splash()
+
+print(logic.declareWinner(db))
